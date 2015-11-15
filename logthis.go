11176affd50030da87782bf5f37abcad7e3da7cb
David Liedle <david.liedle@gmail.com>
2015-11-15 10:51:26 -0700

    setup

--- logthis.go
+++ logthis.go
@@ -1,8 +1,16 @@
-// logthis.go
+/**
+ * logthis.go - GitHub.com/DavidLiedle/loggly-blog-go-logthis
+ * Supporting example code for the Loggly blog post on ""What to Log in Go"
+ * @author David Christian Liedle
+ * @link   http://davidcanhelp.me/
+ */
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
+func main() {
 	fmt.Println("Examples of what to log (and how).")
+	fmt.Println("Here's what we'll log:")
 }
